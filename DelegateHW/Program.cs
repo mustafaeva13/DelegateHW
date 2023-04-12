42cb08a7d2993137594aaead288a055f81940944
efsane <126017552+mustafaeva13@users.noreply.github.com>
2023-04-12 09:03:28 +0400

    Add project files.

---
+++ DelegateHW/Program.cs
@@ -0,0 +1,68 @@
+﻿
+#region Task1
+//namespace PrintMessage
+//{
+//    class Program
+//    {
+//        public delegate void Print(int val);
+
+//        static void ConsolePrint(int i)
+//        {
+//            Console.WriteLine(i);
+//        }
+//       public static void Main(string[] args)
+//        {
+//            Print print = ConsolePrint;
+//            print(10);
+//        }
+
+//    }
+//}
+#endregion
+
+#region Task2
+//namespace Square
+//{
+//    class Program
+//    {
+//        public delegate TResult Func<in T, out TResult>(T arg);
+//        static void Main(string[] args)
+//        {
+//            int Square(int x)
+//            {
+//                return x * x;
+
+//             }
+
+//            Func<int,int> add = Square;
+
+//            int result = add(15);
+
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
+#endregion
+#region Task5
+//namespace Task5
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Action<string> text = new Action<string>(Display);
+//            text("Game World");
+//            Console.Read();
+//        }
+
+//        static void Display(string message)
+//        {
+//            Console.WriteLine(message);
+//        }
+//    }
+//}
+#endregion
+
+
+
+
